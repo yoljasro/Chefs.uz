20e362c9cb736ff369da36f98986465755cfb014
Jasurbek <saidaliyevjasur450@gmail.com>
2023-08-18 16:16:59 +0500

    sites

---
+++ private/back/sites/all/README.txt
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3ecb0c27c91c8d4d4b3c4a2cffae7c378d0e32b170b2aca4a9db0149fd2a0260
+size 319
---
+++ private/back/sites/default/default.settings.php
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:407a7b1501ee4a8f34d6c87f1c0aa2fb3a9c04249fd5e2bc19c833088aca2fd0
+size 10568
