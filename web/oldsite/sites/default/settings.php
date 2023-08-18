f3618504b1433c6113587ade81c0f82ae61885d4
Jasurbek <saidaliyevjasur450@gmail.com>
2023-08-18 17:01:15 +0500

    oldsite

---
+++ web/oldsite/sites/default/settings.php
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3d1d963ea8322db60642cc6637d59daab351518665b44b0fe22b1c691d6b0568
+size 10560
---
+++ web/oldsite/sites/default/settings_loc.php
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:86474778421c9976998b719da10edb795e7960d8db742c738947717e2f25f731
+size 10549
